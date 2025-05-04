c6d550f2c70aa938020fe77866a9292003a654e7
oumouma oumouma <ouma.godwin10@gmail.com>
2025-05-04 17:54:35 +0300

    Rename TodoListScript to TodoScript and improve private key handling in deployment script

--- script/Todo.s.sol
+++ script/Todo.s.sol
@@ -2,18 +2,23 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import { TodoList } from "../src/TodoList.sol";
+import {TodoList} from "../src/TodoList.sol";
 
-contract TodoListScript is Script {
+contract TodoScript is Script {
     TodoList public todoList;
 
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
-
+        // Read the private key from the .env file
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        
+        // Start broadcasting transactions using the private key
+        vm.startBroadcast(deployerPrivateKey);
+        
         todoList = new TodoList();
-
+        console.log("TodoList deployed at:", address(todoList));
+        
         vm.stopBroadcast();
     }
 }
